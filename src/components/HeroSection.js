07688059561ea7ef939bc542b0eacb78a9212b62
willemnielsen <wnielsen@vassar.edu>
2023-02-07 11:37:14 -0500

    Added dark background

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,28 +6,13 @@ export default class HeroSection extends Component {
   render() {
     return (
       <>
-
-        <Container id='hero-section-container' fluid>
-          <Row id='hero-section' className="align-items-end">
-            <Col id='image' md={{ span: 7, order: 2 }}>
-            </Col>
-            <Col id='title' md={{ span: 5, order: 1 }} >
-              <Row>Head Coach Vassar Men's Basketball</Row>
-              <Row>
-                <Button className='hero-button'>ABOUT ME</Button>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
-        {/* <div className='title'>
-              Vassar Men's Basketball Head Coach
-        </div>
-        <div className='other'>
-        </div> */}
-        {/* <a href='/'>
-            <Button>LEARN MORE</Button>
-        </a> */}
+          <Container id='hero-section'>
+            <Row>
+              <Button className='hero-button'>ABOUT ME</Button>
+            </Row>
+            <Row id='title' className='mt-auto'>Head Coach of Vassar Men's Basketball</Row>
+          </Container>
       </>
     )
   }
-}
+} 
