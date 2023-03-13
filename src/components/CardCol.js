62311553c7d40c45b5a3c05c0bc3b663a2f67267
willemnielsen <wnielsen@vassar.edu>
2023-03-13 01:31:26 -0400

    Reformatted cardcol and added buttons

--- src/components/CardCol.js
+++ src/components/CardCol.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Card, Col } from 'react-bootstrap';
+import { Card, Col, Button } from 'react-bootstrap';
+import './CardCol.css';
 
 const CardCol = ({title, text, imgSrc}) => {
   return (
-    <Col style={{ marginTop: '20px'}} md={4} >
+    <Col style={{ marginTop: '20px'}} lg={4} md={5} sm={5}>
       <Card>
         <Card.Img variant="top" src={imgSrc} />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{text}</Card.Text>
+          <Button id='card-button'>
+            Register
+          </Button>
         </Card.Body>
       </Card>
     </Col>
--- src/components/EventSection.js
+++ src/components/EventSection.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
 import { Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import CardRow from './CardRow'
+import './EventSection.css'
 
 export default class EventSection extends Component {
   render() {
     // testing
     return (
-      <Row>
-        <Row className='title' style={{color: 'black'}}>
+      <Row id='event-section'>
+        <Row className='title' style={
+          {color: 'black',
+          marginTop: '30px'}}>
           Upcoming Events
         </Row>
         <CardRow/>
+        <Row>
+          <Link  to='/events' style={{
+            margin: '15px 30px',
+            fontSize: '20px'
+          }}>
+            See All Events 
+          </Link>
+        </Row>
       </Row>
+     
     )
   }
 }
