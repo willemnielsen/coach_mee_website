71b76397b317d3047a6748b15b55e7793ae4b470
willemnielsen <wnielsen@vassar.edu>
2023-03-17 21:17:04 -0400

    removed eventsection for now

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,7 +9,7 @@ export default class Home extends Component {
     return (
       <Container id='home' fluid>
         <HeroSection />
-        <EventSection />
+        {/* <EventSection /> */}
       </Container>
     )
   }
