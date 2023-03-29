958743ca3cf9634b274a7934f25265c53e3ac9e9
willemnielsen <wnielsen@vassar.edu>
2023-03-29 13:54:21 -0400

    added PORT env variable

--- events_api/events.js
+++ events_api/events.js
@@ -3,14 +3,20 @@ const app = express();
 const events = require('./events.json');
 const cors = require('cors');
 
-app.use(cors());
+const corsOpts = {
+  origin: 'http://localhost:3000',
+};
+
+app.use(cors(corsOpts));
 
 app.get('/events', (req, res) => {
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   console.log('GET request received');
   console.log(events);
   res.json(events);
 });
 
-app.listen(3001, () => {
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
   console.log('Server is listening on port 3001');
 });
