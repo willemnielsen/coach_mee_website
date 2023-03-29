de161f40dd6bbfb841f7c42128d884c3b00a681a
willemnielsen <wnielsen@vassar.edu>
2023-03-28 23:18:43 -0400

    Fixed SpringTuneUp styling

--- src/components/SpringTuneUp.js
+++ src/components/SpringTuneUp.js
@@ -1,22 +1,39 @@
-import React from 'react'
-import { Container, Col, Image } from 'react-bootstrap'
+import React from "react";
+import { Container, Col, Image } from "react-bootstrap";
 
 const SpringTuneUp = ({ img1Src, img2Src }) => {
-    console.log(img1Src);
+  console.log(img1Src);
   return (
     <>
-    <Container fluid>
-        
-        <Col>
-        
-            <Image src={`/images/${img1Src}`} fluid />
+      <Container fluid>
+        <Col
+          sm={12}
+          style={{
+            display: "flex",
+            marginTop: "60px",
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            src={`/images/${img1Src}`}
+            style={{ maxWidth: "765px", width: "100%" }}
+          />
         </Col>
-        <Col>
-            <Image src={`/images/${img2Src}`} fluid />
+        <Col
+          sm={12}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Image
+            src={`/images/${img2Src}`}
+            style={{ maxWidth: "760px", width: "100%" }}
+          />
         </Col>
-    </Container>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default SpringTuneUp
+export default SpringTuneUp;
